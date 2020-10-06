b74e591d07d3623367b9be55441792162e9d8f1d
mamaev-v <vova-mamaev@yandex.ru>
2020-10-06 14:43:35 -0500

    Lab5

---
+++ 5.2.py
@@ -0,0 +1,8 @@
+address = input('Введите адрес сети в формате х.х.х.х/х: ')
+address = address.replace('/','.')
+address = address.split('.')
+oct1, oct2, oct3, oct4, mask = address
+
+print(f'''
+Network:
+{oct1:<8} '''
